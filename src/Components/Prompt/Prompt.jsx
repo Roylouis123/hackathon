8bc5fd5e6aec80d0d1ae08612d028810c6e1d8d3
Harikaran M <harikaran@ariveguru.com>
2024-05-24 12:08:23 +0530

    prompt-box

--- src/Components/Prompt/Prompt.jsx
+++ src/Components/Prompt/Prompt.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-
+import "./Prompt.css";
+import SendIcon from "@mui/icons-material/Send";
 const Prompt = () => {
   const [prompt, setPrompt] = useState("");
+
   return (
-    <div>
+    <div className="prompt-container">
       <input
         className="prompt-input"
         type="text"
         onChange={(e) => setPrompt(e.target.value)}
       />
+      <SendIcon className="send-icon" />
     </div>
   );
 };
