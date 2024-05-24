d05066dc031c1fd3c0c5b8a1e404a5dab49c3a5d
Shabid Khan <shabid@ariveguru.com>
2024-05-24 11:27:44 +0530

    added side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/sideBar';
 
 
 function App() {
-
   return (
-    <>
-      <div>
-        hii
-      </div>
-    </>
-  )
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
 }
 
-export default App
+export default App;
