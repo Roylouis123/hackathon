19cc5f42351a92cc71eae899e8949b339da56b91
balakrishna <bala@ariveguru.com>
2024-05-24 18:41:06 +0530

    updated the code editor

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from "./contact";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Sidebar from "./components/Sidebar/sideBar";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Sidebar from "./Components/Sidebar/sideBar";
+import TextEditor from "./Components/TextEditor/TextEditor";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/" element={<Dashboard />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/neweditor" element={<TextEditor />} />
           </Routes>
         </div>
       </div>
